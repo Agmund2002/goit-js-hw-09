5b083f799cd55c349551b9c2667d57ff6a5882f2
Agmund2002 <malysh.andrey2000@gmail.com>
2023-08-18 00:48:01 +0300

    Completed task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
 
+const body = document.querySelector('body');
+const btn = {
+    start: document.querySelector('button[data-start]'),
+    stop: document.querySelector('button[data-stop]')
+}
+
+let idInterval = null;
+btn.stop.disabled = true;
+
+btn.start.addEventListener('click', handlerStart);
+btn.stop.addEventListener('click', handlerStop);
+
+function handlerStart() {
+    btn.start.disabled = true;
+    btn.stop.disabled = false;
+
+    idInterval = setInterval(() => body.style.backgroundColor = getRandomHexColor(), 1000);
+}
+
+function handlerStop() {
+    btn.start.disabled = false;
+    btn.stop.disabled = true;
+
+    clearInterval(idInterval);
+}
